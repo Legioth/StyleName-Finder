5aa9118e43c4d5eab22b36bb77bb00b49c0527de
Leif Åstrand <leif@vaadin.com>
2017-12-19 11:52:46 +0200

    Support Vaadin 8.1+
    Fixes #1

--- stylenamefinder/src/main/java/org/vaadin/leif/stylenamefinder/client/StyleNameFinderInjector.java
+++ stylenamefinder/src/main/java/org/vaadin/leif/stylenamefinder/client/StyleNameFinderInjector.java
@@ -1,15 +1,28 @@
 package org.vaadin.leif.stylenamefinder.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.RunAsyncCallback;
+import com.vaadin.client.ApplicationConfiguration;
 import com.vaadin.client.debug.internal.VDebugWindow;
 
 public class StyleNameFinderInjector implements EntryPoint {
     @Override
     public void onModuleLoad() {
-        VDebugWindow debugWindow = VDebugWindow.get();
-        if (debugWindow != null && debugWindow.isAttached()) {
-            // Not attached if get() returns dummy instance
-            debugWindow.addSection(new StyleNameFinderSection());
+        if (ApplicationConfiguration.isDebugMode()) {
+            // Run when debug mode bundle is loaded
+            GWT.runAsync(VDebugWindow.class, new RunAsyncCallback() {
+                @Override
+                public void onSuccess() {
+                    VDebugWindow.get().addSection(new StyleNameFinderSection());
+                }
+
+                @Override
+                public void onFailure(Throwable reason) {
+                    // Ignore. Failure will be handled by
+                    // ApplicaitonConfiguration
+                }
+            });
         }
     }
 }
